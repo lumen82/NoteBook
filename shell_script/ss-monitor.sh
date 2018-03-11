e41ebab950301652634455b4f456a121f78bc7f0
lumen82 <616568416@qq.com>
2018-03-11 12:56:00 +0800

    add ss monitor

---
+++ shell_script/ss-monitor.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+function GetPID #User #Name
+{
+	PsUser=$1
+	PsName=$2
+	pid=`ps -u $PsUser|grep $PsName|grep -v grep|grep -v vi|grep -v dbx|grep -v tail|grep -v start|grep -v stop|sed -n 1p|awk '{print $1}'`
+	echo $pid
+}
+
+PID=`GetPID lumen sslocal`
+echo $PID
+
+function GetMem #PID
+{
+	MEMUsage=`ps -o vsz -p $1|grep -v VSZ`
+	((MEMUsage /= 1000))
+	echo $MEMUsage
+}
+
+MEMUsage=`GetMem $PID`
+echo $MEMUsage
+
+if [$MEMUsage -gt 400]
+then
+	{
+		`/home/lumen/github/ss-bash/ssadmin.sh restart`
+	}
+
